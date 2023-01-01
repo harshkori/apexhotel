27fbcba43d69078d2bfe6602f3f7a1e810644a3a
harsh <harsh@gmail.com>
2023-01-01 22:49:56 +0530

    First Commit

--- build_files.sh
+++ build_files.sh
@@ -1,10 +1,4 @@
-# build_files.sh
-which python3.q0
-pwd
-cd /usr/local/bin/
-yum install sqlite-devel -y
-./configure --enable-optimizations --enable-loadable-sqlite-extensions
-make && make altinstall
-cd /vercel/path0/
-pip install -r requirements.txt
-python3.10 manage.py collectstatic --noinput
+# build start
+python3.10 -m pip install -r requirements.txt
+python3.10 manage.py collectstatic --noinput --clear
+# build end
--- hotel/settings.py
+++ hotel/settings.py
@@ -122,10 +122,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS=[
-    os.path.join(BASE_DIR,'static')
-]
+# STATIC_URL = '/static/'
+# STATICFILES_DIRS=[
+#     os.path.join(BASE_DIR,'static')
+# ]
 # print (os.path.join(BASE_DIR,'static\css'))
 
 
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR /"static",]
+STATIC_ROOT = BASE_DIR/'staticfiles'
